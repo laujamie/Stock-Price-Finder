4cf51557213608cb1e0401a44a3e5cc5dbdb42d8
Jamie Lau <laujamie5@gmail.com>
2018-08-17 01:41:42 -0400

    added error handling for bad API request

--- stock_price.py
+++ stock_price.py
@@ -13,22 +13,30 @@ params = dict(
 )
 
 resp = requests.get(url=url, params=params)
-print(resp.url)
+# print(resp.url)
 resp_data = resp.json()
+# print(resp_data)
 data = {}
 
-style.use('fivethirtyeight')
+success = False
+style.use('ggplot')
 
-for key in resp_data.keys():
+for key, val in resp_data.items():
+    if 'Error' in key:
+        break
     if 'Time Series' in key:
-        data = resp_data[key]
+        data = val
+        success = True
         break
 
-stock_data = pd.DataFrame.from_dict(data, dtype=float)
-stock_data = stock_data.iloc[:, ::-1]
-print(stock_data)
-
-plt.figure()
-open_prices = stock_data.loc['1. open']
-open_prices.plot()
-plt.show()
+if success:
+    stock_data = pd.DataFrame.from_dict(data, dtype=float)
+    stock_data = stock_data.iloc[:, ::-1]
+    print(stock_data)
+    plt.figure()
+    open_prices = stock_data.loc['1. open']
+    print(open_prices.index)
+    new_plot = open_prices.plot()
+    plt.show()
+else:
+    print('There was an error retrieving your data')
