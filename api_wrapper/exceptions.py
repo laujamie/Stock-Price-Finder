9d4133a1546ea9615e6ee4d3727069646386b4a1
Jamie Lau <36782341+laujamie@users.noreply.github.com>
2018-12-25 17:32:09 -0500

    update docs

--- api_wrapper/exceptions.py
+++ api_wrapper/exceptions.py
@@ -1,4 +1,4 @@
 class MissingApiKey(Exception):
-    """ custom class for no API key for API wrapper """
+    """ custom class for missing API key for API wrapper """
     def __init__(self):
         Exception.__init__(self, "No API Key for AlphaVantage was given")
