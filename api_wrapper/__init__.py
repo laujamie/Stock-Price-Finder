d007d3b799bd75deebd5ab42d61a1b12ecc7d336
Jamie Lau <36782341+laujamie@users.noreply.github.com>
2018-12-26 01:20:36 -0500

    add new Exception for failed API call

--- api_wrapper/__init__.py
+++ api_wrapper/__init__.py
@@ -2,4 +2,4 @@ from .time_series import TimeSeries
 from .digital_currencies import DigitalCurrencies
 from .foreign_exchange import Forex
 from .technical_indicators import TechnicalIndicators
-from .exceptions import MissingApiKey
+from .exceptions import MissingApiKey, ResponseError
--- api_wrapper/exceptions.py
+++ api_wrapper/exceptions.py
@@ -2,3 +2,8 @@ class MissingApiKey(Exception):
     """ custom class for missing API key for API wrapper """
     def __init__(self):
         Exception.__init__(self, "No API Key for AlphaVantage was given")
+
+class ResponseError(Exception):
+    """ custom class for error returned from AlphaVantage API """
+    def __init__(self, *args):
+        Exception.__init__(self, "{}".format(args[0]))
